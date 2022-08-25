8fdbb156186d123d8f203d0da0923475d6793879
Bers <semeikayaskaevih@gmail.com>
2022-08-25 10:45:42 +0300

    initial commit

---
+++ scripts/help.js
@@ -0,0 +1,15 @@
+﻿var startTime = new Date().getTime;
+
+var shpVis = function(){
+  
+}
+
+
+
+var shp = document.getElementById("shape");
+shp.onclick = function(){
+  shp.style.display = "none";
+}
+ 
+
+
